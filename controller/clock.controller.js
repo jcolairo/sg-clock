fc8a294cf18b3b5c2d8e3352867c9bceebde168a
James Colairo <jcolairo@spartaglobal.co>
2017-04-15 10:57:48 +0100

    added basic setup for the clock app

--- controller/clock.controller.js
+++ controller/clock.controller.js
@@ -1,9 +1,21 @@
-function ClockController() {
+function ClockController($timeout) {
   var controller = this;
 
-  
+  controller.tickInterval = 1000;
+
+  var tick = function() {
+    controller.clock = Date.now();
+    $timeout(tick, controller.tickInterval);
+  };
+
+  function init() {
+    $timeout(tick, controller.tickInterval);
+  }
+  init();
 }
+
+ClockController.$inject = ['$timeout'];
 
 angular
   .module('ClockApp')
-  .controller('ClockController', ClockController);
+  .controller('ClockController', ClockController, ['interval']);
